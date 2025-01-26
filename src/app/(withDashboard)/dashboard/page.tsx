bad4ac59c4a73e8174ecb50e32d22d8e5f2d7c17
MD ABDULLAH <MD ABDULLAH>
2025-01-26 11:57:27 +0600

    project setup with nextAuth social login

---
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { authOptions } from "@/utils/authOption";
+import { getServerSession } from "next-auth";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  return (
+    <div>
+      <h1 className="text-4xl text-center mt-10">
+        Welcome {session?.user?.name}
+      </h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ src/utils/authOption.ts
@@ -0,0 +1,23 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID as string,
+    //   clientSecret: process.env.GITHUB_SECRET as string,
+    // }),
+    // ...add more providers here
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXT_AUTH_SECRET,
+};
